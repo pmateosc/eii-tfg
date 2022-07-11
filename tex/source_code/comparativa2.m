bbb4282cd75409979e529236483e766681409628
Pablo Mateos <79842764+pmateosc@users.noreply.github.com>
2022-07-11 14:48:52 +0200

    final version

---
+++ tex/source_code/comparativa2.m
@@ -0,0 +1,97 @@
+n=20;
+h=0.1;
+Tmax=120;
+t=0;
+g=0.75;
+
+XVi=rand(n,4);
+XVi(:,[3:4])=XVi(:,[3:4])*6-3;
+
+F=zeros(n,16);
+
+XV0=zeros(n,4);
+XV_arbor=zeros(n,4);
+XV_trelat1=zeros(n,4);
+XV_trelat2=zeros(n,4);
+
+max0=zeros(101,1);
+max_arbor=zeros(101,1);
+max_trelat1=zeros(101,1);
+max_trelat2=zeros(101,1);
+
+figure(1);
+tamano_pantalla=get(0,'ScreenSize');
+set(gcf, 'Position', [0 0 tamano_pantalla(3) tamano_pantalla(4)]);
+aux=1;
+
+while t<=Tmax
+    if t==0
+        F(:,[1:2])=XVi(:,[3:4]);
+        F(:,[5:6])=XVi(:,[3:4]);
+        F(:,[9:10])=XVi(:,[3:4]);
+        F(:,[13:14])=XVi(:,[3:4]);
+        
+        F(:,[3:4])=cs0(XVi,n,g);
+        F(:,[7:8])=cs_arbor(XVi,n,g);
+        F(:,[11:12])=cs_trelat1(XVi,n,g);
+        F(:,[15:16])=cs_trelat2(XVi,n,g);
+        
+        XV0=XVi+F(:,[1:4])*h;
+        XV_arbor=XVi+F(:,[5:8])*h;
+        XV_trelat1=XVi+F(:,[9:12])*h;
+        XV_trelat2=XVi+F(:,[13:16])*h;   
+       
+    else
+        F(:,[1:2])=XV0(:,[3:4]);
+        F(:,[5:6])=XV_arbor(:,[3:4]);
+        F(:,[9:10])=XV_trelat1(:,[3:4]);
+        F(:,[13:14])=XV_trelat2(:,[3:4]);
+        
+        F(:,[3:4])=cs0(XV0,n,g);
+        F(:,[7:8])=cs_arbor(XV_arbor,n,g);
+        F(:,[11:12])=cs_trelat1(XV_trelat1,n,g);
+        F(:,[15:16])=cs_trelat2(XV_trelat2,n,g);
+
+        XV0=XV0+F(:,[1:4])*h;
+        XV_arbor=XV_arbor+F(:,[5:8])*h;
+        XV_trelat1=XV_trelat1+F(:,[9:12])*h;
+        XV_trelat2=XV_trelat2+F(:,[13:16])*h;
+    end
+    
+    [max0(aux),max_arbor(aux),max_trelat1(aux),max_trelat2(aux)] = dibujar_max_dif(XV0(:,[3:4]),XV_arbor(:,[3,4]),XV_trelat1(:,[3,4]),XV_trelat2(:,[3,4]),n);
+    aux=aux+1;
+   
+    subplot(2,2,1); title('Sin control');
+    quiver(XV0(:,1),XV0(:,2),XV0(:,3),XV0(:,4));
+    
+    subplot(2,2,2); title('Control Arbor');
+    quiver(XV_arbor(:,1),XV_arbor(:,2),XV_arbor(:,3),XV_arbor(:,4));
+    title('Control Arbor');
+    subplot(2,2,3); 
+    quiver(XV_trelat1(:,1),XV_trelat1(:,2),XV_trelat1(:,3),XV_trelat1(:,4));
+    title('Control Trelat 1');
+    subplot(2,2,4); 
+    quiver(XV_trelat2(:,1),XV_trelat2(:,2),XV_trelat2(:,3),XV_trelat2(:,4));
+    title('Control Trelat 2');
+
+       
+    t=t+h;
+    pause(0.1)
+
+end
+figure(2); hold on;
+plot(linspace(0,20,length(max0)),max0);
+plot(linspace(0,20,length(max_arbor)),max_arbor);
+plot(linspace(0,20,length(max_trelat1)),max_trelat1);
+plot(linspace(0,20,length(max_trelat2)),max_trelat2);
+title('Velocidad de consenso');
+legend('max sin control','max arbor','max trelat1','max trelat2');
+figure(3); hold on;
+plot(max0(99:end));
+plot(max_arbor(99:end));
+plot(max_trelat1(99:end));
+plot(max_trelat2(99:end));
+title('Velocidad de consenso ampliada');
+legend('max sin control','max arbor','max trelat1','max trelat2');
+
+save("comparativa2_b075.mat")
